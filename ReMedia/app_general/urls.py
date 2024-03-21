2c80e9f433f3c1eb2f9163a131dde1bc96ea245f
Sarawut <125782682+SarawutEz@users.noreply.github.com>
2024-03-21 14:36:38 +0700

    base

--- ReMedia/app_general/urls.py
+++ ReMedia/app_general/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('Movies', views.Movies, name='Movies'),
+    path('Movie', views.Movie, name='Movie'),
 ]
--- ReMedia/app_general/views.py
+++ ReMedia/app_general/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('helo')
+    return render(request, 'app_general/index.html')
+
+def Movies (request):
+    return render(request, 'app_general/Movie.html')
+
+def Movie (request):
+    return HttpResponse('Movie_id')              
